832bd687124a8efee9cc805a3680c273944a77e6
Ngo Tien Dat <ngotiendat345@icloud.com>
2021-05-14 00:28:18 +0700

    first commit

---
+++ src/components/Block.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="block" v-if="showBlock" @click="stopTimer">click me</div>
+</template>
+
+<script>
+export default {
+  props: ['delay'],
+  data () {
+    return {
+      showBlock: false,
+      reactionTime: 0,
+      timer: null
+    }
+  },
+  methods: {
+    startTimer () {
+      this.timer = setInterval(() => {
+        this.reactionTime += 10
+      }, 10)
+    },
+    stopTimer () {
+      clearInterval(this.timer)
+      this.$emit('end', this.reactionTime)
+      console.log(this.reactionTime)
+    }
+  },
+  mounted () {
+    console.log('mounted ruun')
+    setTimeout(() => {
+      this.showBlock = true
+      this.startTimer()
+    }, this.delay)
+  },
+  updated () {
+    console.log('updated ruun')
+  }
+}
+</script>
+<style>
+.block {
+  width: 400px;
+  border-radius: 20px;
+  background: #0faf87;
+  color: white;
+  text-align: center;
+  padding: 100px 0;
+  margin: 40px auto;
+}
+</style>
---
+++ src/components/Result.vue
@@ -0,0 +1,24 @@
+<template>
+  <p>Reaction time - {{ score }} ms</p>
+  <p v-if="rank">{{ rank }}</p>
+</template>
+
+<script>
+export default {
+  props: ['score'],
+  data () {
+    return {
+      rank: null
+    }
+  },
+  mounted () {
+    if (this.score < 250) {
+      this.rank = 'High level rank'
+    } else if (this.score < 500) {
+      this.rank = 'Nomal level rank'
+    } else {
+      this.rank = 'Low level rank'
+    }
+  }
+}
+</script>
